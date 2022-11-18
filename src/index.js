ff3f1e25ce4902817cc486a81213c4efb7b596d4
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-18 19:34:26 +0530

    corrected mongodb env variable and logic for mongodb connect app.listen

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const app = require("./app.js");
 require("dotenv").config();
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 
 mongoose.connect(MONGODB_URL, { dbName: "instaclone" }, (err) => {
     if (err) console.dir(err);
-    console.log("Connected to the mongoDB cluster");
+    else console.log("Connected to the mongoDB cluster");
 });
 
 app.listen(PORT, (err) => {
     if (err) console.dir(err);
-    console.log("Server listening on port ", PORT);
+    else console.log("Server listening on port ", PORT);
 });
