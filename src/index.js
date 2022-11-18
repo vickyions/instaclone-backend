9beb93a74b2164c5d18510b8981a6e807fdeefa1
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-18 22:13:25 +0530

    added cloudinary upload api and implemented post endpoint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 const app = require("./app.js");
 require("dotenv").config();
+const cloudinary = require('cloudinary').v2;
 
 const MONGODB_URL = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: true
+});
 
 mongoose.connect(MONGODB_URL, { dbName: "instaclone" }, (err) => {
     if (err) console.dir(err);
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,32 +1,76 @@
-const router = require('express').Router();
-const uuidv4 = require('uuid').v4;
+const router = require("express").Router();
+const uuidv4 = require("uuid").v4;
+const fs = require("fs");
+const cloudinary = require("cloudinary").v2;
 
-const multer = require('multer');
-const upload = multer(); //to handle form-data
+const multer = require("multer");
 
-const Post = require('../models/Post');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/tmp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = uuidv4();
+    cb(null, uniqueSuffix + file.originalname);
+  },
+});
+const upload = multer({ storage: storage }); //to handle form-data
+
+//const multipart = require('connect-multiparty')();
+
+const Post = require("../models/Post");
 
 router.get("/", async (_req, res) => {
-    try {
-        const posts = await Post.find({});
-        res.status(200).json({
-            status: "success",
-            message: "Succefully fetched all posts",
-            posts
-        })
-    } catch (err) {
-        console.dir(err);
-        res.status(500).json({
-            status: "failed",
-            message: "server error while fetching the posts"
-        });
-    }
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({
+      status: "success",
+      message: "Succefully fetched all posts",
+      posts,
+    });
+  } catch (err) {
+    console.log("get endpoint error::");
+    console.dir(err);
+    res.status(500).json({
+      status: "failed",
+      message: "server error while fetching the posts",
+    });
+  }
 });
 
-router.post("/", upload.any(),(req, res) => {
-    console.dir(req.body);
-    console.dir(req.files)
-    res.json();
-})
+router.post("/", upload.any(), async (req, res) => {
+  const file = req.files[0];
+  let imageFile = file.path;
+  try {
+    // Upload file to Cloudinary
+    const uploadedImage = await cloudinary.uploader.upload(imageFile, {
+      tags: "instaclone",
+    });
+    //console.log(uploadedImage);
+    //saving post to db
+    const post = await Post.create({
+      ...req.body,
+      PostImage: uploadedImage.secure_url,
+    });
+    res.json(post);
+    //deleting after success
+    fs.unlink(imageFile, (err) => {
+      if (err) console.dir(err);
+      else console.log("deleted file after successfull upload", imageFile);
+    });
+  } catch (err) {
+    console.log("post endpoint error:: ");
+    console.dir(err);
+    res.status(500).json({
+      status: "failed",
+      message: "server error during posting image"
+    });
+    //even if failed delete that file
+    fs.unlink(imageFile, (err) => {
+      if (err) console.dir(err);
+      else console.log("deleted file after some error", imageFile);
+    });
+  }
+});
 
 module.exports = router;
